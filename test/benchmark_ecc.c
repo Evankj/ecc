dd519f7be541ff077c8f34b3afb27fa3e91b49e4
Evankj <evankellyjohnston@gmail.com>
2024-06-10 23:01:42 +1000

    Change benchmark to use new macros

--- test/benchmark_ecc.c
+++ test/benchmark_ecc.c
@@ -7,12 +7,11 @@
 #define ARENA_SIZE 1024 * 1024 * 1024
 #define COMPONENT_SIZE 16
 
-const size_t MAX_ENTITIES = 1;
+const size_t BENCHMARK_ENTITIES = 1;
 
 typedef struct {
   int data[4]; // Example component data structure
 } ExampleComponent;
-
 
 void RunBenchmark(int iterations) {
 
@@ -24,43 +23,39 @@ void RunBenchmark(int iterations) {
   }
 
   // fprintf(stdout, "Creating bucket...\n");
-  // fprintf(stdout, "Remaining arena size: %d\n", arena->capacity - arena->top);
-  Bucket *bucket = BucketCreate(arena, MAX_ENTITIES);
+  // fprintf(stdout, "Remaining arena size: %d\n", arena->capacity -
+  // arena->top);
+  Bucket *bucket = BucketCreate(arena, BENCHMARK_ENTITIES);
   if (!bucket) {
     fprintf(stderr, "Failed to create bucket\n");
     return;
   }
 
   // fprintf(stdout, "Registering component...\n");
-  ComponentType *componentType =
-      BucketRegisterComponentType(bucket, sizeof(ExampleComponent));
-  if (!componentType) {
-    fprintf(stderr, "Failed to register component type\n");
-  }
+  // ComponentType *componentType =
+  //     BucketRegisterComponentType(bucket, sizeof(ExampleComponent));
+  // if (!componentType) {
+  //   fprintf(stderr, "Failed to register component type\n");
+  // }
 
   clock_t start = clock();
-  
+
   // fprintf(stdout, "Beginning benchmark...\n");
   for (int i = 0; i < iterations; i++) {
 
     // fprintf(stdout, "Iteration %d\n", i);
     size_t entityId = BucketCreateEntity(bucket);
 
+    Entity *entity = bucket->entities[entityId];
+
     // fprintf(stdout, "Creating component...\n");
-    ExampleComponent *component =
-        ArenaAllocate(arena, sizeof(ExampleComponent));
-    if (!component) {
-      fprintf(stderr, "Failed to allocate component\n");
-    }
+    ExampleComponent *component = ADD_COMPONENT_TO_ENTITY(
+        bucket, entity, ExampleComponent);
 
     component->data[0] = i;
 
-    // fprintf(stdout, "Adding component to entity...\n");
-    AddComponentToEntityById(bucket, entityId, componentType, component);
-
     // fprintf(stdout, "Retrieving component from entity %d...\n", entityId);
-    ExampleComponent *retrievedComponent =
-        GetComponentForEntityById(bucket, entityId, componentType);
+    ExampleComponent *retrievedComponent = GET_COMPONENT_FROM_ENTITY(bucket, entity, ExampleComponent);
 
     if (!retrievedComponent ||
         retrievedComponent->data[0] != component->data[0]) {
